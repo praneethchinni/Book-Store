e17192d8332ec7b98e21763e4c48a74cba40c776
Naga Praneeth Kumar Chinni <84979442+praneethchinni@users.noreply.github.com>
2024-06-29 11:50:04 +0530

    Update config.js
    Changed the Mongodb's URL

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,6 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-    'mongodb+srv://praneethkumarch5:123ASD@traine.ajwm3gg.mongodb.net/<database-name>?retryWrites=true&w=majority&appName=traine&tls=true&tlsAllowInvalidCertificates=true'
+    'mongodb+srv://username:password@traine.ajwm3gg.mongodb.net/<database-name>?retryWrites=true&w=majority&appName=traine&tls=true&tlsAllowInvalidCertificates=true'
+
+//please enter you Mongodb userdetails in the above URL
